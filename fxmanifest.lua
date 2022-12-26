28674ca964fe97e7e74d5ba499559d48a3d424e0
Marshy <84200284+Marshxan@users.noreply.github.com>
2022-12-26 23:18:24 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version "cerulean"
 game "gta5"
 
 author "Marshy"
-description "Flash Warrant Card."
+description "Flash Warrant Card. - https://marshy.tebex.io"
 version '0.0.1'
 
 client_scripts {
